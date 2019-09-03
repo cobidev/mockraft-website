af0a27dc653a423f840b6c1df32ba7862a5c10ea
Jacobo Martinez <jacobojavier98@gmail.com>
2019-09-03 10:42:12 -0400

    Add fade animation to 'Regresar' link on Project Component

--- src/components/RichText.jsx
+++ src/components/RichText.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import Fade from 'react-reveal/Fade';
 
 const RichText = ({ title, subtitle, children }) => (
-  <section className="section-spacing">
+  <section className='section-spacing'>
     <Container>
       <Row>
-        <Col md="12">
+        <Col md='12'>
           <article>
             {!title ? (
               ''
@@ -15,11 +15,13 @@ const RichText = ({ title, subtitle, children }) => (
               <div>
                 {/* Return link */}
                 {subtitle && (
-                  <Link
-                    to="/portafolio"
-                    className="d-inline-block primary-color font-small mb-5">
-                    <u>Regresar</u>
-                  </Link>
+                  <Fade bottom duration={600} delay={1500} distance={'30px'}>
+                    <Link
+                      to='/portafolio'
+                      className='d-inline-block primary-color font-small mb-5'>
+                      <u>Regresar</u>
+                    </Link>
+                  </Fade>
                 )}
                 {/* Title */}
                 <Fade bottom duration={500} delay={500} distance={'30px'}>
@@ -33,12 +35,12 @@ const RichText = ({ title, subtitle, children }) => (
                 {/* Subtitle */}
                 {subtitle && (
                   <Fade bottom duration={500} delay={800} distance={'15px'}>
-                    <h2 className="section-subtitle text-main">{subtitle}</h2>
+                    <h2 className='section-subtitle text-main'>{subtitle}</h2>
                   </Fade>
                 )}
                 {/* Divider */}
                 <Fade bottom duration={500} delay={800} distance={'15px'}>
-                  <span className="span-divider mt-4 mb-5" />
+                  <span className='span-divider mt-4 mb-5' />
                 </Fade>
               </div>
             )}
